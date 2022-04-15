b69d389d1dc9b1cf5a1aa36d9e4e7ac016dac1e9
ᴍʀ. ʀᴀᴍᴇꜱʜᴡᴀʀ ꜱᴀʜᴜ <102160977+Rameshwarsahu07@users.noreply.github.com>
2022-04-15 09:02:46 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+ $conn = mysqli_connect("localhost","root","","crud") or die("connection failed");
+
+?>
---
+++ delete-inline.php
@@ -0,0 +1,15 @@
+<?php 
+echo $stu_id = $_GET['id'];
+
+
+
+    include 'config.php';
+   
+    $sql = "DELETE FROM student WHERE sid = {$stu_id}";
+    $result = mysqli_query($conn,$sql) or die("Query Unsuccessful.");
+
+     header("Location: http://localhost/crud_html/CRUD/index.php");
+
+     mysqli_close($conn);
+
+?>
